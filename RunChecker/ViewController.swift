a831e826d41c45196205d294322362f96a17ce37
EvgeniyLugo <evgeniy.lugovoy@yandex.ru>
2019-11-19 08:55:25 +0300

    Initial Commit

---
+++ RunChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunChecker
+//
+//  Created by Evgeniy Lugovoy on 19.11.2019.
+//  Copyright © 2019 MeadowsPhoneTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
